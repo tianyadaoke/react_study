aaab46472c6dd68f3cd6bdfa685d3ee127fa367d
Bing Zhou <bingfbi@gmail.com>
2021-08-05 13:03:33 +0200

    路由的基本使用

--- react_staging/07_src_路由的基本使用/App.jsx
+++ react_staging/07_src_路由的基本使用/App.jsx
@@ -1,20 +1,22 @@
 import React, { Component } from 'react'
 import { Link, Route } from 'react-router-dom'
-import Header from './components/Header'
-import About from './pages/About'
-import Home from './pages/Home'
+import About from './components/About'
+import Home from './components/Home'
 
 export default class App extends Component {
 
   render() {
     return (
       <div>
-          <Header/>
+        <h2>React Router Demo</h2>
+  
           <Link to="/home">Home</Link>
           <p></p>
           <Link to="/about">About</Link>  
           <Route path="/about" component={About}></Route>
           <Route path="/home" component={Home}></Route>
+  
+
       </div>
     )
   }
---
+++ react_staging/src/pages/About/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    console.log('about组件收到的props是',this.props)
+    return (
+      <div>
+        <h3>我是About的内容</h3>
+      </div>
+    )
+  }
+}
